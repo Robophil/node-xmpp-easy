0394dae97d965a557f0831d708d657752e588cc7
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-28 14:04:39 +0100

    added sample client to test connection to server

---
+++ test-client.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+var debug = console.log
+var xmpp = require('node-xmpp-server');
+var Client = require('node-xmpp-client');
+var Stanza = require('node-xmpp-core').Stanza;
+var IQ = require('node-xmpp-core').IQ;
+
+
+var client1 = new Client({
+    websocket: { url: `ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}` },
+    jid: 'client1@localhost',
+    password: 'secret'
+})
+client1.on('online', function (data) {
+    debug('client1 is online')
+    debug('client1', data)
+    client1.send(new Stanza('message', { to: 'client2@localhost' }).c('body').t('HelloWorld'))
+})
+client1.on('stanza', function (stanza) {
+    debug('client1', 'received stanza', stanza.root().toString())
+})
+client1.on('error', function (error) {
+    debug('client1 auth failed')
+    debug('client1', error)
+  })
+
+
+
+var client2 = new Client({
+    websocket: { url: `ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}` },
+    jid: 'localhost',
+    password: 'notsecret'
+  })
+  client2.on('error', function (error) {
+    debug('client2 auth failed')
+    debug('client2', error)
+  })
+  client2.on('online', function (data) {
+    debug('client2 is online')
+    debug('client2', data)
+})
+
+console.log(`connected to server through websockts on ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}`)
