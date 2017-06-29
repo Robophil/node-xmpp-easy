2505b2cc7bfee923317d5169772c6d90a866e5c3
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-29 13:59:35 +0100

    now sends message in interval

--- test-client.js
+++ test-client.js
@@ -7,23 +7,25 @@ var Stanza = require('node-xmpp-core').Stanza;
 var IQ = require('node-xmpp-core').IQ;
 
 
-// var client1 = new Client({
-//     websocket: { url: `ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}` },
-//     jid: 'client1@localhost',
-//     password: 'secret'
-// })
-// client1.on('online', function (data) {
-//     debug('client1 is online')
-//     debug('client1', data)
-//     client1.send(new Stanza('message', { to: 'client2@localhost' }).c('body').t('HelloWorld'))
-// })
-// client1.on('stanza', function (stanza) {
-//     debug('client1', 'received stanza', stanza.root().toString())
-// })
-// client1.on('error', function (error) {
-//     debug('client1 auth failed')
-//     debug('client1', error)
-// })
+var client1 = new Client({
+    websocket: { url: `ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}` },
+    jid: 'client1@localhost',
+    password: 'secret'
+})
+client1.on('online', function (data) {
+    debug('client1 is online')
+    debug('client1', data)
+    setInterval(()=>{
+        client1.send(new Stanza('message', { to: 'client2@localhost' }).c('body').t('HelloWorld'))
+    }, 1000)
+})
+client1.on('stanza', function (stanza) {
+    debug('client1', 'received stanza', stanza.root().toString())
+})
+client1.on('error', function (error) {
+    debug('client1 auth failed')
+    debug('client1', error)
+})
 
 
 
@@ -41,7 +43,7 @@ client2.on('online', function (data) {
     debug('client2', data)
 })
 client2.on('stanza', function (stanza) {
-    debug('client2', 'received stanza', "yay!!", stanza.root().toString())
+    debug('client2', 'received stanza ==>',  stanza.root().toString(), "\n\n")
 })
 
 console.log(`connected to server through websockts on ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}`)
