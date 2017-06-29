08ee600a16afb07400782ed3d66b099c540b90b5
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-29 12:42:34 +0100

    add and remove client on connect and disconnect respectively

--- event/custom/online.js
+++ event/custom/online.js
@@ -1,7 +1,11 @@
+const CLIENT = require('../../util/clients')
 /**
  * This is called when a client comes online
  * @param client
  */
 module.exports = client => () => {
     console.log('server:', client.jid.local, 'ONLINE')
+    const clientKey = client.jid.user + '@' + client.jid.domain + '/' + client.jid.resource
+    //store client informaiton
+    CLIENT.set(clientKey, client)
 }
