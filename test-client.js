62b974d966cf659b93f4f88f5e79c90f4ead16fe
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-29 12:34:37 +0100

    commented out one client

--- test-client.js
+++ test-client.js
@@ -7,38 +7,41 @@ var Stanza = require('node-xmpp-core').Stanza;
 var IQ = require('node-xmpp-core').IQ;
 
 
-var client1 = new Client({
-    websocket: { url: `ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}` },
-    jid: 'client1@localhost',
-    password: 'secret'
-})
-client1.on('online', function (data) {
-    debug('client1 is online')
-    debug('client1', data)
-    client1.send(new Stanza('message', { to: 'client2@localhost' }).c('body').t('HelloWorld'))
-})
-client1.on('stanza', function (stanza) {
-    debug('client1', 'received stanza', stanza.root().toString())
-})
-client1.on('error', function (error) {
-    debug('client1 auth failed')
-    debug('client1', error)
-  })
+// var client1 = new Client({
+//     websocket: { url: `ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}` },
+//     jid: 'client1@localhost',
+//     password: 'secret'
+// })
+// client1.on('online', function (data) {
+//     debug('client1 is online')
+//     debug('client1', data)
+//     client1.send(new Stanza('message', { to: 'client2@localhost' }).c('body').t('HelloWorld'))
+// })
+// client1.on('stanza', function (stanza) {
+//     debug('client1', 'received stanza', stanza.root().toString())
+// })
+// client1.on('error', function (error) {
+//     debug('client1 auth failed')
+//     debug('client1', error)
+// })
 
 
 
 var client2 = new Client({
     websocket: { url: `ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}` },
-    jid: 'localhost',
+    jid: 'client2@localhost',
     password: 'notsecret'
-  })
-  client2.on('error', function (error) {
+})
+client2.on('error', function (error) {
     debug('client2 auth failed')
     debug('client2', error)
-  })
-  client2.on('online', function (data) {
+})
+client2.on('online', function (data) {
     debug('client2 is online')
     debug('client2', data)
+})
+client2.on('stanza', function (stanza) {
+    debug('client2', 'received stanza', "yay!!", stanza.root().toString())
 })
 
 console.log(`connected to server through websockts on ws://${process.env.XMPP_DOMAIN}:${process.env.XMPP_PORT}`)
